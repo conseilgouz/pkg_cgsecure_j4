b11d52088a7b0e0741330ec9a2641a09fafba3b6
Pascal Leconte <pascal.leconte@conseilgouz.com>
2026-04-22 13:36:47 +0200

    3.7.8
    Password may not contain % & + symbols

---
+++ packages/com_cgsecure/admin/src/Rule/PassRule.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @component     CG Secure
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
+ * @copyright (C) 2026 ConseilGouz. All Rights Reserved.
+ * @author ConseilGouz
+**/
+
+namespace ConseilGouz\Component\CGSecure\Administrator\Rule;
+defined( '_JEXEC' ) or die( 'Restricted access' );
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Form\Form;
+use Joomla\CMS\Form\Rule\PasswordRule;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\Registry\Registry;
+
+class PassRule extends PasswordRule
+{
+
+	public function test(\SimpleXMLElement $element, $value, $group = null, Registry $input = null,Form $form = null) {
+
+        if (!empty($value)) {
+           $nHits = preg_match_all('/[%&+]/', $value, $imatch);
+           if ($nHits > 0) {
+                Factory::getApplication()->enqueueMessage(Text::_('CGSECURE_INVALID_SPECIALS'),'error');
+                return false;
+           }
+        }
+        return parent::test($element, $value, $group, $input,$form);
+		
+	}
+}
