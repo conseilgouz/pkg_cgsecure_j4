3ac9f50ec2a165d8803ac7253eb3b50acf28c1a7
Pascal Leconte <pascal.leconte@conseilgouz.com>
2026-04-27 15:31:35 +0200

    3.8.1

--- packages/plg_system_cgsecure/htaccess.php
+++ packages/plg_system_cgsecure/htaccess.php
@@ -138,9 +138,7 @@ if (isset($_SERVER['REDIRECT_STATUS'])) {
         $err = 'Security key failure'.$line;
     }
 } else {
-    $req = htmlspecialchars($_SERVER['REQUEST_URI'], ENT_QUOTES);
-    $req = substr($req, 0, 300); // limit message length
-    $err = "Direct access to plugin not allowed. Req : ".$req;
+    $err = "Direct access to plugin not allowed.";
 }
 $tmp .= $err.$block.'</h3></body></html>';
 echo $tmp;
