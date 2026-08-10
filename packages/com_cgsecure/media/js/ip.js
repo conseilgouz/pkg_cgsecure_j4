d2e7354139768a631a317672999f0542d67c7825
Pascal Leconte <pascal.leconte@conseilgouz.com>
2026-08-10 11:19:35 +0200

    4.0.2
    Don't auto add IP V6 in white list

--- packages/com_cgsecure/media/js/ip.js
+++ packages/com_cgsecure/media/js/ip.js
@@ -32,11 +32,6 @@
 		$ip = data['ip'];
         if (!$ip) return;
 		$result.innerHTML = $html+','+$ip;
-		$html = document.getElementById("jform_whitelist").value;
-		if ($html.trim() == '') 
-            document.getElementById("jform_whitelist").value = $ip;
-        else if ($html.indexOf($ip) < 0)
-            document.getElementById("jform_whitelist").value = $html.trim()+','+$ip;
 		$html = document.getElementById("jform_country").value;
 		$lang = navigator.language;
 		if ($lang.length != 2) {
